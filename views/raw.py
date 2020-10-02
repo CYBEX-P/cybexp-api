8c728e740f936c2537561367ec3290c64793a5a2
samuelgerard <48971286+samuelgerard@users.noreply.github.com>
2020-10-02 00:16:47 -0700

    Completed docstrings for raw.py

--- views/raw.py
+++ views/raw.py
@@ -6,10 +6,63 @@ import pymongo
 import loadconfig
 from . import crypto
 
+#where we input data
+
 class Raw(object):
-    file_entries, fs = loadconfig.get_cache_db()
+    """
+    'Raw' is a class that works as the front line of the API.
+    When called, it pulls the database config information and issues a timestamp index.
+    The 'Raw' object then takes raw unfiltered data that is passed to it by the input database calling the on_post() function.
+    That data is then scanned for indicated fields and then sent to the cache database.
+
+    Attributes
+    ----------
+    file_entries: cache database reference
+        Responsible for timestamping the data entry and sending the Input Config and raw data to the cache database provided 
+        it passes the check for all required keys being present
+    
+    fs: cache database reference to the fs
+        Responsible for handling the raw data and deleting the contents of the 'info' if the input does not pass the required
+        keys check
+    
+    r:  timestamp reference
+        Holds the timestamp
+        
+
+
+    """
+    file_entries, fs = loadconfig.get_cache_db() 
+    r = file_entries.create_index("timestamp")
 
     def on_post(self, req, resp):
+        """
+        Handles the posting of new raw unfiltered data and then sends it to the cache database.
+
+        Parameters
+        ----------
+        req: Input Cofig Object
+            The Input Configuration object. Mandatory values include:
+            'typetag', 'name', 'orgid', 'timezone', 'fid' along with the unfiltered data.
+        
+        resp: Falcon Request Object
+            Handles the HTTP responses
+
+
+        Raises
+        ------
+        falcon.errors.HTTPBadRequest
+            Invalid or Incorrect raw input
+       
+        pymongo.errors.ServerSelectionTimeoutError
+            Cache data lake is down
+        
+        KeyboardInterrupt
+            Exit the system
+        
+        General Exception
+            Server Error
+
+        """
         try:
 
             info = {}
@@ -19,14 +72,15 @@ class Raw(object):
             for part in req.media:
                 if part.name == 'file':
                     fenc = crypto.encrypt_file(part.stream.read())
-                    info['fid'] = self.fs.put(fenc, filename=part.filename)
-                elif part.name in ['typetag', 'name', 'orgid', 'timezone']:
+                    info['fid'] = self.fs.put(fenc, filename=part.filename) 
+                elif part.name in ['typetag', 'name', 'orgid', 'timezone','config_hash']:
                     info[part.name] = part.text
                 else:
                     resp.media = {"message": "Invalid input: " + part.name}
                     resp.status = falcon.HTTP_400
                     return
 
+           
             required_keys = ['typetag', 'name', 'orgid', 'timezone', 'fid']
             if not all(key in info for key in required_keys):
                 resp.media = {"message": "Incomplete input"}
@@ -34,8 +88,9 @@ class Raw(object):
                 if 'fid' in info and self.fs.exists(info['fid']):
                     self.fs.delete(info['fid'])
                 return              
-
-            _ = self.file_entries.insert_one(info)
+            
+            
+            _ = self.file_entries.insert_one(info) 
             resp.media = {"message" : "File Uploaded Successfully"}
             resp.status = falcon.HTTP_201
           
