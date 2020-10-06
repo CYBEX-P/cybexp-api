3e22680f2c13b22d0f01b44b5b06ff1431b2c684
Ignacio Astaburuaga <ignaciochg@hotmail.com>
2020-10-06 16:30:53 -0700

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-import api
-
-# # Replace with your app's method of configuration
-# config = myproject.get_config(os.environ['MYPROJECT_CONFIG'])
-# # uWSGI will look for this variable
-# application = myproject.create_api(config)
-
-application = api.app
