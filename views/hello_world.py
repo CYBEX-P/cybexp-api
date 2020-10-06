361c0969d51c869f71410314234ac892c9ab7ecc
Ignacio Astaburuaga <ignaciochg@hotmail.com>
2020-10-06 16:51:33 -0700

    Update hello_world.py

--- views/hello_world.py
+++ views/hello_world.py
@@ -3,7 +3,7 @@ import time
 import logging
 
 
-class hello_world(object):
+class HelloWorld(object):
     def on_get(self, req, resp):
         resp.status = falcon.HTTP_200  # This is the default status
         resp.body = ('hello world ;)\n')
