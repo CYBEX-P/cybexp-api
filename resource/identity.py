69a9e0196e2c220c6750c63c5fa82d5fe82d0d44
Ignacio Astaburuaga <ignaciochg@hotmail.com>
2020-10-22 13:55:11 -0700

    Update identity.py

--- resource/identity.py
+++ resource/identity.py
@@ -220,10 +220,10 @@ class ChangeACL(object):
 class GetMyHash(object):
     def __init__(self, ident_backend):
         self.ident_backend = ident_backend #required by decorator
-    def on_post(self, req, resp):
+    def on_get(self, req, resp):
         return self.get_my_hash(req,resp)
 
-    @ident_common.extract_request_data()
+    #@ident_common.extract_request_data()
     @ident_common.validate_token
     @ident_common.exception_handler
     def get_my_hash(self, req, resp, user_object, **kwargs):
